d0b05ff3d509c840bf7ed9b0e16bca89e4d6e30c
AthulyaShan <140399053+AthulyaShan@users.noreply.github.com>
2023-07-25 11:36:50 +0530

    Update streamlit_app.py
    Update code to choose the fruits by name!!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas 
 
-st.title('My Parents New Healthy Diner')
+st.title('My Mom's New Healthy Diner')
 
 st.header('Breakfast Menu')
 
@@ -11,6 +11,7 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
